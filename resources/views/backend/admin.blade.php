3a5ba58125e06605f0e9a602e95b535815c7426b
monirpust <mdmonir1997@gmail.com>
2021-12-06 11:08:38 +0600

    admin template added

--- resources/views/backend/admin.blade.php
+++ resources/views/backend/admin.blade.php
@@ -1,2 +1,15 @@
+
 <x-app-layout>
 </x-app-layout>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  @include('backend.style')
+  </head>
+  <body>
+
+    @include('backend.sidebar')
+    @include('backend.script')
+  </body>
+</html>
